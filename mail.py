7e756cbe07b7866f02280ebbf5fc7729726fdc56
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2020-06-05 22:13:19 +0600

    Email address checker
    Still have some bud. Exp. input = abc@.  output = found which is not true

---
+++ mail.py
@@ -0,0 +1,12 @@
+import re
+while True:
+    g_mail = input("Enter your G-mail: ")
+    if not g_mail:
+        break
+    def srch():
+        pat = r"@(.)*\." # (.) mean any character, * Zero or more time of the expression,
+        if re.search(pat, g_mail):
+            print("Found")
+        else:
+            print("Incorrect gmail address")
+    srch()
